cbb90dd0fb93bb3d7d618cc8d203faa9ba96064b
oneturkmen <oneturkmen@gmail.com>
2023-12-10 20:42:37 -0500

    Améliorer la structure du code

--- 10/sol2.py
+++ 10/sol2.py
@@ -8,9 +8,7 @@ from collections import deque
 """
 import sys
 print(sys.getrecursionlimit())
-sys.setrecursionlimit(1000000000)
-import threading
-threading.stack_size(134217728)   # for your needs
+sys.setrecursionlimit(100000000)
 
 def read_lines():
     with open("input_large.txt") as file:
@@ -24,6 +22,7 @@ def bounds_ok(i, j, n, m):
     return 0 <= i < n and 0 <= j < m
 
 class Solution:
+
     def expand_grid(self, grid):
         n = len(grid)
         m = len(grid[0])
@@ -39,10 +38,8 @@ class Solution:
                     expanded_grid[i][j] = '!'
 
         return expanded_grid
-
-    def solution(self, grid):
-        grid = self.expand_grid(grid)
-
+    
+    def find_start_pos(self, grid):
         n = len(grid)
         m = len(grid[0])
         s_i, s_j = 0, 0
@@ -57,7 +54,11 @@ class Solution:
             if found:
                 break
 
-        self.step = 0
+        return s_i, s_j
+
+    def valid_move(self, i, j, c, direction, n, m):
+        if not (i >= 0 and i < n and j >= 0 and j < m):
+            return False
 
         valid_dirs = {
             "F": ["bottom", "right"],
@@ -69,38 +70,75 @@ class Solution:
             "=": ["left", "right"],
             "!": ["up", "bottom"],
         }
-        #steps = [[0 for c in range(m)] for r in range(n)]
-        def valid(i, j, c, direction):
-            if not (i >= 0 and i < n and j >= 0 and j < m):
-                return False
 
-            return direction in valid_dirs.get(c, [])
+        return direction in valid_dirs.get(c, [])
 
-        def init_start(s_i, s_j, n, m):
-            start_positions = []
+    def init_queue(self, grid):
+        n = len(grid)
+        m = len(grid[0])
+        positions = []
 
-            if bounds_ok(s_i - 2, s_j, n, m) and grid[s_i - 2][s_j] in {'F', '|', '7'}:
-                grid[s_i-1][s_j] = '#'
-                start_positions.append([s_i - 2, s_j])
+        for i in range(n):
+            # Add first and last columns
+            if grid[i][0] != '#':
+                positions.append((i, 0))
+            if grid[i][m-1] != '#':
+                positions.append((i, m-1))
 
-            if bounds_ok(s_i + 2, s_j, n, m) and grid[s_i + 2][s_j] in {'J', '|', 'L'}:
-                grid[s_i+1][s_j] = '#'
-                start_positions.append([s_i + 2, s_j])
+        for j in range(m):
+            # Add first and last rows
+            if grid[0][j] != '#':
+                positions.append((0, j))
+            if grid[n-1][j] != '#':
+                positions.append((n-1, j))
 
-            if bounds_ok(s_i, s_j - 2, n, m) and grid[s_i][s_j - 2] in {'F', '-', 'L'}:
-                grid[s_i][s_j-1] = '#'
-                start_positions.append([s_i, s_j - 2])
+        return positions
+    
+    def get_starting_positions_for_walking(self, s_i, s_j, grid):
+        n = len(grid)
+        m = len(grid[0])
+        start_positions = []
 
-            if bounds_ok(s_i, s_j + 2, n, m) and grid[s_i][s_j + 2] in {'J', '-', '7'}:
-                grid[s_i][s_j+1] = '#'
-                start_positions.append([s_i, s_j + 2])
+        if bounds_ok(s_i - 2, s_j, n, m) and grid[s_i - 2][s_j] in {'F', '|', '7'}:
+            grid[s_i-1][s_j] = '#'
+            start_positions.append([s_i - 2, s_j])
 
-            return start_positions
+        if bounds_ok(s_i + 2, s_j, n, m) and grid[s_i + 2][s_j] in {'J', '|', 'L'}:
+            grid[s_i+1][s_j] = '#'
+            start_positions.append([s_i + 2, s_j])
 
+        if bounds_ok(s_i, s_j - 2, n, m) and grid[s_i][s_j - 2] in {'F', '-', 'L'}:
+            grid[s_i][s_j-1] = '#'
+            start_positions.append([s_i, s_j - 2])
+
+        if bounds_ok(s_i, s_j + 2, n, m) and grid[s_i][s_j + 2] in {'J', '-', '7'}:
+            grid[s_i][s_j+1] = '#'
+            start_positions.append([s_i, s_j + 2])
+
+        return start_positions
+
+    def flood_fill(self, queue, grid):
+        n = len(grid)
+        m = len(grid[0])
+        while len(queue) > 0:
+            i, j = queue.pop(0)
+            for di, dj in [[0, -1], [-1, 0], [1, 0], [0, 1]]:
+                ni, nj = i + di, j + dj
+                if ni >= 0 and ni < n and nj >= 0 and nj < m and grid[ni][nj] != '#':
+                    grid[ni][nj] = '#'
+                    queue.append((ni, nj))
+        return grid
+
+    def solution(self, grid):
+        grid = self.expand_grid(grid)
+        s_i, s_j = self.find_start_pos(grid)
+
+        q = deque()
+        n = len(grid)
+        m = len(grid[0])
 
         grid[s_i][s_j] = '#'
-        q = deque()
-        q.extend(init_start(s_i, s_j, n, m))
+        q.extend(self.get_starting_positions_for_walking(s_i, s_j, grid))
         while len(q) > 0:
             i, j = q.popleft()
             if grid[i][j] == '#':
@@ -110,54 +148,26 @@ class Solution:
             grid[i][j] = '#'
 
             # Left
-            if valid(i, j - 1, c, "left") and grid[i][j - 1] in ["F", "L", "-", "="]:
+            if self.valid_move(i, j - 1, c, "left", n, m) and grid[i][j - 1] in ["F", "L", "-", "="]:
                 q.append((i, j - 1))
             # Bottom
-            elif valid(i + 1, j, c, "bottom") and grid[i + 1][j] in ["J", "L", "|", "!"]:
+            elif self.valid_move(i + 1, j, c, "bottom", n, m) and grid[i + 1][j] in ["J", "L", "|", "!"]:
                 q.append((i + 1, j))
             # Right
-            elif valid(i, j + 1, c, "right") and grid[i][j + 1] in ["J", "7", "-", "="]:
+            elif self.valid_move(i, j + 1, c, "right", n, m) and grid[i][j + 1] in ["J", "7", "-", "="]:
                 q.append((i, j + 1))
             # Up
-            elif valid(i - 1, j, c, "up") and grid[i - 1][j] in ["7", "F", "|", "!"]:
+            elif self.valid_move(i - 1, j, c, "up", n, m) and grid[i - 1][j] in ["7", "F", "|", "!"]:
                 q.append((i - 1, j))
 
-        def init_queue():
-            positions = []
-
-
-            for i in range(n):
-                # Add first and last columns
-                if grid[i][0] != '#':
-                    positions.append((i, 0))
-                if grid[i][m-1] != '#':
-                    positions.append((i, m-1))
-
-            for j in range(m):
-                # Add first and last rows
-                if grid[0][j] != '#':
-                    positions.append((0, j))
-                if grid[n-1][j] != '#':
-                    positions.append((n-1, j))
-
-            return positions
-            
-        
-        queue = init_queue()
-
-        while len(queue) > 0:
-            i, j = queue.pop(0)
-            for di, dj in [[0, -1], [-1, 0], [1, 0], [0, 1]]:
-                ni, nj = i + di, j + dj
-                if ni >= 0 and ni < n and nj >= 0 and nj < m and grid[ni][nj] != '#':
-                    grid[ni][nj] = '#'
-                    queue.append((ni, nj))
-
-        walked_grid = [''.join(row) for row in grid]
+        # Flooding! 
+        queue = self.init_queue(grid)
+        grid = self.flood_fill(queue, grid)
 
         ans = 0
-        for row in walked_grid:
-            ans += len(list(filter(lambda c : c != '#' and c != ' ' and c != '!' and c != '=', row)))
+        ignored_chars = set(['#', ' ', '!', '='])
+        for row in [''.join(row) for row in grid]:
+            ans += len(list(filter(lambda c : c not in ignored_chars, row)))
 
         return ans
 
